45fae9396bb846e420ee44c9d2fd7b952597c703
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2024-07-06 18:52:18 +0300

    First day of MVC masterclass bootcamp.

---
+++ WebApplication1/WebApplication1/Controllers/bookController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class bookController : Controller
+    {
+        public Book book = new Book();
+        // GET: book
+        public ActionResult Index()
+        {
+            Book book = createBook();
+
+            return View(book);
+        }
+
+        private Book createBook()
+        {
+            Author author = createAuthor();
+            Book book = new Book();
+            book.author = author;
+            book.Name = "Clean Code";
+            book.Desc = "Made read learn how to write clean, effective code.";
+            book.page = 464;
+
+            return book;
+        }
+        private Author createAuthor()
+        {
+
+            Author author = new Author();
+
+            author.Name = "Robert C. Martin";
+            author.BirthYear = 1952;
+
+            return author;
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Adı")]
+        public string Name { get; set; }
+
+        [DisplayName("Özet")]
+        public string Desc { get; set; }
+
+        [Required]
+        [DisplayName("Sayfa sayısı")]
+        [MinLength(50, ErrorMessage = "Eklenecek kitabın sayfa sayısı en az 50 olmalıdır.")]
+        [MaxLength(350, ErrorMessage = "Eklenecek kitabın sayfa sayısı en fazla 350 olmalıdır.")]
+        public int page { get; set; }
+
+        [Required]
+        [DisplayName("Yazar")]
+        public Author author { get; set; }
+
+        /*     [Required]
+             [DisplayName("Yazar Adı")]
+             public string Author { get; set; }*/
+    }
+}

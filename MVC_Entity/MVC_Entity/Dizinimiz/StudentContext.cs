8a96cda7e2d49602681b5529849c57e5d0185f66
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2024-07-13 20:46:49 +0300

    Worked on MVC Entity Framework. Created Student CRUD process. Connected to MSSQL

---
+++ MVC_Entity/MVC_Entity/Dizinimiz/StudentContext.cs
@@ -0,0 +1,22 @@
+﻿using MVC_Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Entity.Dizinimiz
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(): base("StudentDatabase") { }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ MVC_Entity/MVC_Entity/Models/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Entity.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Name")] 
+        public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DisplayName("TC No")]
+        [MinLength(11, ErrorMessage = "TC Kimlik Numarası 11 karakter olmalıdır.")]
+        [MaxLength(11, ErrorMessage = "TC Kimlik Numarası 11 karakter olmalıdır.")]
+        public string TcNo { get; set; }
+
+        public string Major { get; set; }
+        public int RegisteredYear { get; set; }
+
+        [Required]
+        [DisplayName("Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+    }
+
+}
+
+

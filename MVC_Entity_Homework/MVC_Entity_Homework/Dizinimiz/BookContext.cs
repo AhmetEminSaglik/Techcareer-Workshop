9a072455c437ffd7e3be78d7d417264e978e5a29
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2024-07-13 20:47:50 +0300

    Homework is completed. Created CRUD process with Entity Framework of Book and Author classes.

---
+++ MVC_Entity_Homework/MVC_Entity_Homework/Dizinimiz/BookContext.cs
@@ -0,0 +1,16 @@
+﻿using MVC_Entity_Homework.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Entity_Homework.Dizinimiz
+{
+    public class BookContext : DbContext
+    {
+        public BookContext() : base("BookDatabase") { }
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
---
+++ MVC_Entity_Homework/MVC_Entity_Homework/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Entity_Homework.Models
+{
+    public class Book
+    {
+
+        public int Id { get; set; }
+        [DisplayName("Title")]
+        public string Title { get; set; }
+        [DisplayName("Description ")]
+        public string Desc { get; set; }
+
+        [DisplayName("Page ")]
+        public int Page { get; set; }
+
+        [DisplayName("Author Full Name")]
+        public string AuthorName { get; set; }
+    }
+
+}

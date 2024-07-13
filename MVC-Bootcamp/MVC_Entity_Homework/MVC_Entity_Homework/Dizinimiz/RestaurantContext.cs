612309f2c0abbea543ad8b2386247960aa4b53d9
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2024-07-14 00:45:30 +0300

    Restaurant Entity Framework  homework is completed

---
+++ MVC-Bootcamp/MVC_Entity_Homework/MVC_Entity_Homework/Dizinimiz/RestaurantContext.cs
@@ -0,0 +1,25 @@
+﻿using MVC_Entity_Homework.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Entity_Homework.Dizinimiz
+{
+    public class RestaurantContext : DbContext
+    {
+        public RestaurantContext() : base("RestaurantDatabase") { }
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+        /*
+         *  This is used to remove 's' which adds to the database tables' name.
+         *  
+         * protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }*/
+    }
+}
---
+++ MVC-Bootcamp/MVC_Entity_Homework/MVC_Entity_Homework/Models/Restaurant.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Entity_Homework.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        [Required]
+        [MinLength(1, ErrorMessage = "Must includes minimum 1 characters.")]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+        [Required]
+        [DisplayName("Address")]
+        [MinLength(10, ErrorMessage = "Must includes minimum 10 characters.")] 
+        public string Address { get; set; }
+        [Required]
+        [DisplayName("City")]
+        [MinLength(1, ErrorMessage = "Must includes minimum 1 characters.")]
+        public string City { get; set; }
+        [Required]
+        [DisplayName("Region")]
+        [MinLength(1, ErrorMessage = "Must includes minimum 1 characters.")]
+        public string Region { get; set; }
+        [Required]
+        [DisplayName("Postal Code")]
+        public string PostalCode { get; set; }
+        [Required]
+        [DisplayName("Phone Number")]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [DisplayName("Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
